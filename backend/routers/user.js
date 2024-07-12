34b18622f5b7325e497218b0a747d6da5bbd1dbb
Ankit Kumar <itsankit.k27@gmail.com>
2024-07-12 11:45:42 +0530

    register and login modified

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -7,13 +7,13 @@ const jwt = require('jsonwebtoken');
 const config = require('../config');
 
 router.post('/register', async (req, res) => {
-    const { name, email, password, role } = req.body;
+    const { name, email, password } = req.body;
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await prisma.user.create({
-            data : { name, email, password: hashedPassword, role }
+            data : { name, email, password: hashedPassword }
         })
-        res.status(201).json(utils.createSuccess(newUser));
+        res.status(201).json(utils.createSuccess({ name: newUser.name, email: newUser.email }));
     } catch (error) {
         res.status(500).json(utils.createError('User registration failed'));
         console.error(error);
@@ -34,7 +34,7 @@ router.post('/login', async (req, res) => {
             return res.status(401).json(utils.createError('Invalid email or password'));
         }
         const token = jwt.sign({ id: user.id, role: user.role }, config.secret);
-        res.status(200).json(utils.createSuccess({ id: user.id, name: user.name, email, token  }));
+        res.status(200).json(utils.createSuccess({ id: user.id, name: user.name, email, role: user.role, token  }));
     } catch (error) {
         res.status(500).json(utils.createError('Login failed'));
     }
